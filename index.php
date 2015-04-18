2b7397d75d1ef8f99ff7448d746525efcd2a36c6
Austin Juntra <AustinJuntra@gmail.com>
2015-04-17 20:27:14 -0700

    video 16

--- index.php
+++ index.php
@@ -18,9 +18,9 @@
 						$task_id = $row['id'];
 						$task_name =$row["task"];
 						echo '<li>
-						<span>'.$task_name'
-						';
-						
+						<span>'.$task_name. '</span>
+						<img id="'.$task_id.'" class="delete-button" width="10px" src="images/close.svg"/>
+						</li>';
 					}
 				}
 			}
