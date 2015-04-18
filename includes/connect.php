1842c55132b86cd191cc9e98a0549251d26d59db
Austin Juntra <AustinJuntra@gmail.com>
2015-04-17 19:47:12 -0700

    video 7 new css

---
+++ includes/connect.php
@@ -0,0 +1,13 @@
+<?php
+//why is this important? why does localhost have to be first?
+$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+//$mysqli->connectP)
+if ($mysqli->connect_error) {
+	die('Connect Error (' . $mysqli_>connect_errno . ')' 
+	  . $mysqli->connect_error);
+}
+else {
+	echo "Connection made";
+}
+$mysqli->close();
+?>
